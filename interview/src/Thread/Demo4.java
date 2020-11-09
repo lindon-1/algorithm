bca0ba9bbb30d83c2b774815aa91ecf83aa5ca05
lindonglin <1904098977@qq.com>
2020-11-09 16:30:42 +0800

    交替打印100内数字例子

---
+++ interview/src/Thread/Demo4.java
@@ -0,0 +1,31 @@
+package Thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Description 交替打印100内数字
+ * @Author Lindonglin
+ * @date 2020/11/9 16:27
+ **/
+public class Demo4 {
+
+    public static void main(String[] args) {
+        AtomicInteger num = new AtomicInteger(0);
+        new Thread(() -> {
+            while (num.get() < 100) {
+                if (num.get() % 2 == 0) {
+                    System.out.println(Thread.currentThread().getName() + ":" + num.get());
+                    num.getAndIncrement();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            while (num.get() < 100) {
+                if (num.get() % 2 == 1) {
+                    System.out.println(Thread.currentThread().getName() + ":" + num.get());
+                    num.getAndIncrement();
+                }
+            }
+        }).start();
+    }
+}
