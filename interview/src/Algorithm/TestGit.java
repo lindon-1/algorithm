54c05a08de81c97343c8f79fa47213dee2983b17
lindon-1 <31766269+lindon-1@users.noreply.github.com>
2021-07-27 14:57:39 +0800

    Update TestGit.java
    update gitTest

--- interview/src/Algorithm/TestGit.java
+++ interview/src/Algorithm/TestGit.java
@@ -2,6 +2,7 @@ package Algorithm;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("testGit");
+        System.out.println("testG");
+        System.out.println("SSS");
     }
 }
